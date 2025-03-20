60dbc5e1fa49ec06b9f0de2af75b9a26f22caf95
Vidhi Sethi <sethi.vidhi@gmail.com>
2025-03-20 23:55:29 +0530

    investor login/register donesies

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,12 +1,15 @@
 import express from "express";
 import { entreLogin,entreRegister } from "../controllers/entreControllers.js";
 import { verifyToken } from "../middleware/verifyToken.js";
+import { InvestorLogin, InvestorRegister } from "../controllers/investorController.js";
 
 const router=express.Router();
 
 //entrepreneur routes
 router.post("/entre/register",entreRegister);
 router.post("/entre/login",entreLogin);
+router.post("/investor/register", InvestorRegister);
+router.post("/investor/login", InvestorLogin);
 // router.get("/details", verifyToken, details);
 
 
