7d2879fe5a5d6bb238e19fdf7d367126e83acd86
mihira4 <mihirag04@gmail.com>
2025-03-21 05:08:55 +0530

    added search entre

--- backend/routes/searchRoute.js
+++ backend/routes/searchRoute.js
@@ -2,10 +2,11 @@ import express from "express";
 import { searchInvestor } from "../controllers/searchInvestor.js";
 import { searchInvestorDomain } from "../controllers/searchInvestor.js";
 import { verifyToken } from "../middleware/verifyToken.js";
-
+import { searchEntre } from "../controllers/searchEntre.js";
 const router=express.Router();
 
 router.get("/investor", verifyToken, searchInvestor);
 router.get("/investor/:domain", verifyToken, searchInvestorDomain);
+router.get("/entre",verifyToken,searchEntre)
 
 export default router;
